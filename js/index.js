f2dd3c5f8641ac8e221e3ca196fa38e8926fb599
naefun <nathan_byrne96@hotmail.co.uk>
2021-11-06 21:59:25 +0000

    popup adjustments

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@
 var btns = document.getElementsByClassName("showcase-info-button");
 btns[0].addEventListener("click", (e) => {
     document.getElementById("popup-container").style.display = "flex";
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
 });
 
 // close button
 var windowCloseBtn = document.getElementById("window-close-button");
 windowCloseBtn.addEventListener("click", (e) => {
     document.getElementById("popup-container").style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
 })
 
 // minimise button
@@ -15,7 +17,6 @@ var windowMinimiseBtn = document.getElementById("window-minimise-button");
 windowMinimiseBtn.addEventListener("click", (e) => {
     document.getElementById("minimise-text").style.whiteSpace = "nowrap";
     document.getElementById("minimise-text").style.textOverflow = "ellipsis";
-    document.getElementById("minimise-text").style.width = "100%";
     document.getElementById("minimise-text").style.overflow = "hidden";
 })
 
@@ -23,6 +24,5 @@ windowMinimiseBtn.addEventListener("click", (e) => {
 var windowMaximiseBtn = document.getElementById("window-maximise-button");
 windowMaximiseBtn.addEventListener("click", (e) => {
     document.getElementById("minimise-text").style.whiteSpace = "normal";
-    document.getElementById("minimise-text").style.width = "100%";
     document.getElementById("minimise-text").style.overflow = "inherit";
 })
